30edc6dffde64c379b9c4ad22bd0e812f83dac50
Marlon Marin <marlonmariin97@outlook.com>
2023-05-20 15:24:45 -0500

    Slight modifications from a backup folder

--- app/main.py
+++ app/main.py
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-import charts
-
-def run():
-    labels = ['Colombia', 'Argentina', 'Brazil']
-    values = [200, 34, 120]
-    charts.generate_bar_chart(labels, values)
-=======
 import pandas as pd
 import charts
 
@@ -14,11 +6,10 @@ def run():
     region = input('Enter a region =>')
     df = pd.read_csv('../data/countries.csv')
     df = df[df['Continent'] == region]
-    
+        
     countries = df['Country'].values
     percentages = df['World Population Percentage'].values
     charts.generate_pie_chart(region, countries, percentages)
->>>>>>> 2a28d8b (Pandas use in former app project in use in former app project from Python 102)
 
 if __name__ == '__main__':
     run()
