ce4c1268cdcd80fdd94287175bd6f32c3774f589
Marlon Marin <marlonmariin97@outlook.com>
2023-05-20 20:35:28 -0500

    check_option() function summarizes previously defined user_option() and pc_option() also assert was deleted. By other side, main pitfall was corrected, consisted of computer_win and user_win initialization in 0 inside rules() function

--- game.py
+++ game.py
@@ -2,50 +2,39 @@
 
 import random
 
-def pc_option():
-    options = ['paper', 'scissors', 'rock']
-    chosen_option_pc = options[random.randint(0, 2)]
-    return chosen_option_pc
-
-def user_option():
-    options = ['paper', 'scissors', 'rock']
-    chosen_option_user = input('Choose an option: ').lower()
-    try:
-        assert chosen_option_user in options, 'Choose an option between paper, scissors, and rock'
-    except AssertionError as e:
-        print(e)
-    return chosen_option_user
-
-def rules(pc_option, user_option):
+def check_option():
     
-    computer_win = 0
-    user_win = 0
+    options = ['paper', 'scissors', 'rock']
+    chosen_option_pc = random.choice(options)
+    chosen_option_user = input('Choose paper, scissors, or rock => ').lower()
     
+    if chosen_option_user not in options:
+        print('choose a valid option!')
+        return None, None
+        
+    return chosen_option_pc, chosen_option_user
+
+def rules(pc_option, user_option, computer_win, user_win):
+  
     if user_option == pc_option:
         print('DRAW!')
         
     elif user_option == 'rock':
         if pc_option == 'scissors':
-            print('user wins!')
             user_win += 1
         elif pc_option == 'paper':
-            print('computer wins!')
             computer_win += 1
     
     elif user_option == 'scissors':
         if pc_option == 'rock':
-            print('computer wins!')
             computer_win += 1
         elif pc_option == 'paper':
-            print('user wins!')
             user_win += 1
             
     elif user_option == 'paper':
         if pc_option == 'scissors':
-            print('computer wins!')
             computer_win += 1
         elif pc_option == 'rock':
-            print('user wins!')
             user_win += 1
 
     return computer_win, user_win
@@ -53,28 +42,32 @@ def rules(pc_option, user_option):
 
 def run():
     
-    # print('*'*100)
-    # print('WELCOME TO PAPER, ROCK, SCISSORS')
-    rounds = 0
+    computer_win = 0
+    user_win = 0
+    rounds = 1
     
     while True:
-
         
-        pc = pc_option()
-        user = user_option()
+        print('*'*50)
+        print(f'Round number {rounds}')
         
-        computer, you = rules(pc, user)
+        
+        print('computer_wins', computer_win)
+        print('user_wins', user_win)
         rounds += 1
-        print(computer, you)
         
-        if computer == 2:
+        
+        pc, user = check_option()
+        computer_win, user_win = rules(pc, user, computer_win, user_win)
+        
+        if computer_win == 2:
             print('COMPUTER WON!')
             break
-        elif you == 2:
+        elif user_win == 2:
             print('YOU WON!')
             break
-        
-        print('*'*100)
     
+        print('*'*50)
+        
 if __name__ == '__main__':
     run()
